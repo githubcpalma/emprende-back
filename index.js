cf1fbecf3e884161c37ead21e015b1145cb0585f
KaKUni <mail_cesar_palma@yahoo.com>
2023-08-20 13:32:33 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http")
+
+function requestController(){
+  console.log("Recibimos una nueva request")
+}
+
+const server =  http.createServer(requestController)
+
+server.listen(4000)
